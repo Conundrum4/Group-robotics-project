e37aa2eb83603db7c6c0ec068c625e5b2358f02d
Conundrum4 <35451972+Conundrum4@users.noreply.github.com>
2018-02-05 11:34:24 +0000

    Update avoid.py

--- avoid.py
+++ avoid.py
@@ -11,39 +11,67 @@ def steering(msg):
     #np.isnan(msg.ranges[719])
     #print msg.ranges[719]
     
-    left = msg.ranges[180:]	
+    left = msg.ranges[:180]	
     #print 'left' + left
-    front = msg.ranges[351:370]
+    front = msg.ranges[181:440]
     #print 'front' + front
-    right = msg.ranges[:441]
+    right = msg.ranges[441:]
     #print 'right' + right
     
 
-
-    #If left is closer than right, turn right
-    if min(left) < min(right) and min(front) >= 1.5:
-        print 'right'
-        message = Twist(Vector3(0.1,0,0), Vector3(0,0,-1.0))
-       
-    #If left is less than 1.5m from the wall
-    elif min(left) < 0.5 and min(front) < 0.5:
+    #
+    if min(left) < 0.9 and min(front) >= 1.0 :
+        # If it's close to the wall, go forward
+        # and keep closing to the wall
+        message = Twist(
+            Vector3(0.1, 0, 0),
+            Vector3(0, 0, 0.45)
+        )
         
-        message = Twist(Vector3(0,0,0), Vector3(0,0,-1.0))
-    
-    #If right is closer than left, turn left
-    if min(right) < min(left) and min(front) >= 1.5:
+    if min(right) < 0.9 and min(front) >= 1.0 :
+        # If it's close to the wall, go forward
+        # and keep closing to the wall
+        message = Twist(
+            Vector3(0.1, 0, 0),
+            Vector3(0, 0, -0.45)
+        )
         
-        message = Twist(Vector3(0.1,0,0), Vector3(0,0,1.0))
-    
-    #If right is less than 1.5m from the wall
-    elif min(right) < 0.5 and min(front) < 0.5:
+    elif min(left) < 0.5 :
+        # If the robot is very close to the wall,
+        # only rotates to the other side
+        message = Twist(
+            Vector3(0, 0, 0),
+            Vector3(0, 0, -0.25)
+        )
         
-        message = Twist(Vector3(0,0,0), Vector3(0,0,-1.0))
+    elif min(right) < 0.5 :
+        # If the robot is very close to the wall,
+        # only rotates to the other side
+        message = Twist(
+            Vector3(0, 0, 0),
+            Vector3(0, 0, 0.25)
+        )
         
-    else:
-        print 'else'
-        message = Twist(Vector3(0.2,0,0), Vector3(0,0,0))
-    
+    elif min(left) < 2.0 and min(front) < 2.0 :
+        # If it's closing to the wall,
+        # slows the velocity and rotate agressively
+        message = Twist(
+            Vector3(0.1, 0, 0),
+            Vector3(0, 0, 0.65)
+        )
+        
+    elif min(right) < 2.0 and min(front) < 2.0 :
+        message = Twist(
+            Vector3(0.1, 0, 0),
+            Vector3(0, 0, -0.65)
+        )
+        
+    else :
+        # Move forward
+        message = Twist(
+            Vector3(0.2,0,0),
+            Vector3(0,0,0)
+        )
     
     pub.publish(message)
 
@@ -57,4 +85,6 @@ pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size = 1)
 sub = rospy.Subscriber('/scan', LaserScan, steering)
 
 #Block until shutdown
-rospy.spin()
+r = rospy.Rate(10)
+while not rospy.is_shutdown():
+    r.sleep()
