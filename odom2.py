c083d86275b08ee15e94a00362c8d96c8cee204c
jrome5 <35452154+jrome5@users.noreply.github.com>
2018-02-11 18:46:30 +0000

    Update odom2.py

--- odom2.py
+++ odom2.py
@@ -38,7 +38,7 @@ speed = Twist()
 # set up the odometry reset publisher
 reset_odom = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
 
-# reset odometry (these messages take a few iterations to get through)
+# reset odometry values (these messages take a few iterations to get through)
 timer = time()
 while time() - timer < 0.5:
     reset_odom.publish(Empty())
