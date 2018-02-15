7ef28fddba47ba6e3c27c91cbc9f2c0d1de08eef
jrome5 <35452154+jrome5@users.noreply.github.com>
2018-02-15 15:06:52 +0000

    Update window2.py

--- window2.py
+++ window2.py
@@ -27,6 +27,8 @@ sub_goal.y = 0
 
 #set the vector increments for a 0.5m radius
 def turn_options(index):
+  global current_x
+  global current_y
   turn = Point()
   x = 0
   y = 0
@@ -51,17 +53,18 @@ def turn_options(index):
   if index == 6:
         x = current_x + 0.13
         y = current_y - 0.48
-   
+
   turn.x = x
   turn.y = y
   return turn
-        
+
 #Obtain the shortest distance to the goal for a paticular set of co-ords
 def dist_to_goal(x,y):
+    global goal
     vect = sqrt(pow((goal.x -x),2) + pow((goal.y-y),2))
     #print ("Distance to goal: %s" %(vect))
     return vect
-        
+
 #Is there an obstacle within 0.7m of the robot on a chosen path?  If yes, eliminate that path as an option
 def check_ranges(distance):
     if distance < 0.7:
@@ -70,6 +73,7 @@ def check_ranges(distance):
 
 #Find the x and y co-ords of the goal
 def find_angle(goal_x,goal_y, x, y):
+    global goal
     inc_x = goal.x - x
     inc_y = goal.y - y
     #use tan to find the angle needed to turn towards the goal
@@ -77,60 +81,64 @@ def find_angle(goal_x,goal_y, x, y):
     #convert angle to degrees
     angle_to_goal = angle_to_goal*(180/pi)
     return angle_to_goal
-   
-  
+
+
 #set the ranges attributed to each option
 def steering(data):
-    
+    global sub_goal
     laser_ranges = [] #an array to store the range values
     new_coords = [] #an array to store potential new co-ords to move to
     no_obstruction = [] #an array to store viable new co-ords (no obstruction present)
     closest = [] #an array to store the distances of the new co-ords from the goal
-    
+
     #In gazebo left to right, on robot right to left
-    zero = data.ranges[431:511] 
-    one = data.ranges[361:430] 
-    two = data.ranges[291:360] 
-    three = data.ranges[224:290] 
-    four = data.ranges[154:223] 
-    five = data.ranges[83:153] 
+    zero = data.ranges[431:511]
+    one = data.ranges[361:430]
+    two = data.ranges[291:360]
+    three = data.ranges[224:290]
+    four = data.ranges[154:223]
+    five = data.ranges[83:153]
     six = data.ranges[0:82]
 
     #an array of the ranges
     laser_ranges = [zero, one, two, three, four, five, six]
     i = 0
     j = 0
-    
-    
+
+
     for i in range(7):
-      new_coords.append(turn_options[i])                                              #adds the new co-ords to the array
-      closest.append(dist_to_goal(turn_options[i].x, dist_to_goal(turn_options[i].y))) #adds distance to goal
-        
-      if min(laser_ranges[i]) < 0.7:                                               #checks if there is an obstruction
+      new_coords.append(turn_options(i))                                              #adds the new co-ords to the array
+      closest.append(dist_to_goal(turn_options(i).x, turn_options(i).y)) #adds distance to goal
+
+      if min(laser_ranges[i]) > 0.7:                                               #checks if there is an obstruction
           no_obstruction.append(1)                                                #This is a viable option
       else:
-          no_obstruction.append(0)                                               #There is an obstruction present
-    
+          no_obstruction.append(0)
+          closest[i] = closest[i]*20
+                                                      #There is an obstruction present
+
     for j in range(7):
-      if laser_ranges[i] == 1 and closest[i] == min(closest):                         #checks laser ranges and dist to goal
-          sub_goal = new_coords[i]                                                   #sets subgoal to co-ords with no obstructions...
+      if (no_obstruction[j] == 1) and (closest[j] == min(closest)):
+          print j                       #checks laser ranges and dist to goal
+          sub_goal = new_coords[j]                                                   #sets subgoal to co-ords with no obstructions...
       else:                                                                           #...is closest to the goal
           print 'nowhere to go!'
-         
-                     
-                     
+
+
 #Odometry callback
 def newOdom(msg):
-    
+    global current_x
+    global current_y
+    global current_th
     current_x = msg.pose.pose.position.x     #set global variable
     current_y = msg.pose.pose.position.y     #set global variable
-                     
+
     orient = msg.pose.pose.orientation       #local variable
     th = 0.0
     (roll, pitch, th) = euler_from_quaternion([orient.x, orient.y, orient.z, orient.w])
-    
+
     current_th = th *(180/pi)                 #set global variable and convert from radians to degrees
-     
+
 #set up nodes
 rospy.init_node("speed_controller", anonymous = True)
 sub = rospy.Subscriber("/odom", Odometry, newOdom)
@@ -140,24 +148,20 @@ speed = Twist()
 #opub = rospy.Publisher("odom", Odometry, queue_size =1)
 # set up the odometry reset publisher
 reset_odom = rospy.Publisher('/mobile_base/commands/reset_odometry', Empty, queue_size=10)
-scan_sub = rospy.Subscriber('/scan', LaserScan, steering) 
-                     
+scan_sub = rospy.Subscriber('/scan', LaserScan, steering)
+
 # reset odometry values (these messages take a few iterations to get through)
 timer = time()
 while time() - timer < 0.25:
     reset_odom.publish(Empty())
-r = rospy.Rate(0.5)
+r = rospy.Rate(5)
 
-                     
-                     
-                     
 #Main method
 while not rospy.is_shutdown():
 
 #obtain the x,y vector to goal
-    inc_x = goal.x - current_x
-    inc_y = goal.y - current_y
-
+    inc_x = sub_goal.x - current_x
+    inc_y = sub_goal.y - current_y
 #use tan to find the angle needed to turn towards the goal
     angle_to_goal = atan2(inc_y, inc_x) #tanx = O/A
 
@@ -166,21 +170,20 @@ while not rospy.is_shutdown():
 
 #find the difference between the angle of the bot and angle needed to turn
     angle = angle_to_goal - current_th
-    print ("x: %s y: %s" % (inc_x, inc_y))
-
+    print ("x: %s y: %s th: %s" % (current_x, current_y, current_th))
+    print angle
 #check if the bot is within a suitable angle to the goal
-    if angle > 1 or angle < -1:
+    if angle > 2 or angle < -2:
         speed.linear.x = 0.0
-        if(angle < -1):
+        if(angle < -2):
             speed.angular.z = -0.5
-        if angle > 1:
+        if angle > 2:
             speed.angular.z = 0.5
-    if -1 <= angle <= 1:
-        print ("vel: %s turn: %s" %(vel,turn))
+    if -2 <= angle <= 2:
         speed.linear.x = 0.3
-        speed.angular.z = 3*turn
+        speed.angular.z = 0.0
 #check if the bot has reached the goal
-    if -0.1 < inc_x < 0.1 and -0.1 < inc_y < 0.1:
+    if -0.1 <= inc_x <= 0.1 and -0.1 <= inc_y <= 0.1:
 	    speed.linear.x = 0
 	    speed.angular.z = 0
     if dist < 0.2:
@@ -188,11 +191,11 @@ while not rospy.is_shutdown():
         print "nuh uh"
     pub.publish(speed)
     r.sleep()
-rospy.spin()                          
-                     
-                     
-                     
-                     
+rospy.spin()
+
+
+
+
 #BEGIN DWA(robotPose,robotGoal,robotModel)
 #   desiredV = calculateV(robotPose,robotGoal)
 #   laserscan = readScanner()
